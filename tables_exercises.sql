2212bae076cf6bb962dfd271bc0777319b163a22
Chris Teceno <chris.teceno@gmail.com>
2022-01-06 15:31:49 -0600

    add tables_exercises.sql

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -3,3 +3,14 @@ SHOW TABLES;
 DESCRIBE employees;
 
 SHOW CREATE TABLE dept_manager;
+
+-- which tables do you think contain which data types?
+-- expect numeric in salaries, dept_manager, employees, salaries, titles; most common use for employee number
+
+-- expect strings in all
+
+-- expect date in employees, dept_emp, dept_manager, salaries, titles
+
+
+-- What is the relationship between the employees and the departments tables?
+-- employeees and there department relationship are stored in dept-emp table and new entries can be added for dept changes
